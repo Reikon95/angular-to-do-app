927923e167b83e7fef14fa971b04503c9473df9a
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-05-09 17:07:16 +0100

    config

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.use(express.static("./"));
 
 //Listen to the port
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log(
     "Express server listening on port %d in %s mode",
     this.address().port,
