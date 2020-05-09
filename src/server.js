87fa47edc790db5552735a4366048f93af66fbc4
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-05-09 16:39:39 +0100

    heroku

---
+++ src/server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var todoController = require("./controllers/todocontroller");
+var app = express();
+
+// set up template engine
+app.set("view engine", "ejs");
+
+//static files
+app.use(express.static("./public"));
+
+//Listen to the port
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
